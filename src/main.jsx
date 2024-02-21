aa670b4b09330f031e7123ebe68483abaf924579
Igor <igglike@gmail.com>
2024-02-21 18:03:41 +0600

    Сделал store с одним редьсером и хуками useSelector/useDispatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,38 @@
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
+
+const defaultState = {
+  counter: 100,
+  todos: [],
+};
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+// фукция reducer - для изменения состояния в сторе
+const counterReducer = (state = defaultState, action) => {
+  // action - действие, которое влияет на изменение состояния
+  // action - объект, у которого обязательное поле type
+  // { type: 'INCREMENT_COUNTER' }
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return {...state, counter: state.counter + 1};
+    case "DECREMENT_COUNTER":
+      return {...state, counter: state.counter - 1};
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,12 +1,32 @@
+import { useSelector, useDispatch } from "react-redux"
+
 
 const CounterPage = () => {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT_COUNTER' })
+  }
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT_COUNTER' })
+  }
+
   return (
     <div className="w-full flex flex-col justify-center gap-3">
       <span className='text-2xl col-span-full'>Счетчик</span>
       <div className="w-full flex justify-center items-center gap-4">
-        <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">-</button>
-        <span>0</span>
-        <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">+</button>
+        <button 
+          className="w-8 bg-slate-300 px-2 py-1 rounded-md"
+          onClick={decrement}
+          >-</button>
+        <span>{counter}</span>
+        <button
+          onClick={increment}
+          className="w-8 bg-slate-300 px-2 py-1 rounded-md">
+            +
+        </button>
       </div>
     </div>
   )
